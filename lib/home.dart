1323076750991c4cb6e758eae5f5366971e9d9e9
Bernardo Amaral <bernardo.amaral85@gmail.com>
2025-08-25 01:32:29 -0300

    Add function to call condominium data

--- lib/home.dart
+++ lib/home.dart
@@ -19,9 +19,8 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   late String totalApartments = '20';
-  late String totalTodayAppointments = '0';
 
-  Future<List<Condominium>> _fetchBuildings() async {
+  Future<List<Condominium>> _fetchCondominiums() async {
     final url = ApiService.endpoint("/condominiums");
     final response = await http.get(url);
 
@@ -30,6 +29,18 @@ class _HomePageState extends State<HomePage> {
       return data.map((item) => Condominium.fromJson(item)).toList();
     } else {
       throw Exception("Erro ao listar edifícios");
+    }
+  }
+
+  Future<List> _fetchCondominiumsData(uuid) async {
+    final url = ApiService.endpoint("/condominiums/$uuid");
+    final response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      final List<dynamic> data = jsonDecode(response.body);
+      return data.map((item) => Condominium.fromJson(item)).toList();
+    } else {
+      throw Exception("Erro ao buscar dados do condomínio");
     }
   }
 
@@ -43,7 +54,7 @@ class _HomePageState extends State<HomePage> {
           Container(
             margin: const EdgeInsets.all(10),
             child: DropdownSearch<Condominium>(
-              items: (filter, cs) => _fetchBuildings(),
+              items: (filter, cs) => _fetchCondominiums(),
               popupProps: const PopupProps.menu(
                 fit: FlexFit.loose,
               ),
@@ -57,6 +68,15 @@ class _HomePageState extends State<HomePage> {
               compareFn: (Condominium a, Condominium b) => a.uuid == b.uuid,
               onChanged: (Condominium? value) {
                 if (value != null) {
+                  _fetchCondominiumsData(value.uuid).then((data) {
+                    setState(() {
+                      totalApartments =
+                          data.isNotEmpty ? data[0].totalApartments : '0';
+                    });
+                  }).catchError((error) {
+                    debugPrint("Erro ao buscar dados do condomínio: $error");
+                  });
+
                   debugPrint(
                       "Selecionado: ${value.uuid} - ${value.description}");
                 }
